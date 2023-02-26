c6eadc39b35717088d139cbe5b5f30022b54fa6f
Callum Spiller <callumjohnspiller@gmail.com>
2023-02-26 15:15:31 +0000

    fixed test

--- ui/src/components/Header/index.spec.tsx
+++ ui/src/components/Header/index.spec.tsx
@@ -4,7 +4,7 @@ import Header from './index';
 test.use({ viewport: { width: 500, height: 500 } });
 
 test('header should be visible', async ({ mount }) => {
-    const component = await mount(<Header navPosition={"center"}/>);
+    const component = await mount(<Header />);
     await expect(component).toBeVisible();
     await expect(component).toHaveId("page-header");
 });
