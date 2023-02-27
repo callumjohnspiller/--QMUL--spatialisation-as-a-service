dd5154401b4a2b47b5fe02f7638951007d6b27bc
Callum Spiller <callum.spiller@sony.com>
2023-02-27 12:20:55 +0000

    debugging

--- ui/src/components/Body/index.tsx
+++ ui/src/components/Body/index.tsx
@@ -9,15 +9,15 @@ interface BodyProps {
     getMessage: Function
 }
 
-function Body(props: BodyProps) {
+async function Body(props: BodyProps) {
     const [uploadStatus, setUploadStatus] = useState<boolean>(false);
     const [sqsQueueUrl, setQueueUrl] = useState<string>();
     const [originalFileUrl, setFileUrl] = useState<string>();
 
     if (uploadStatus && !sqsQueueUrl) {
-        setQueueUrl(props.createSQSQueue().QueueUrl);
+        setQueueUrl(await props.createSQSQueue().QueueUrl);
         console.log(sqsQueueUrl)
-        const s3Message = props.getMessage(sqsQueueUrl);
+        const s3Message = await props.getMessage(sqsQueueUrl);
         setFileUrl("https://" + s3Message[1].detail.bucket.name + ".s3.eu-west-2.amazonaws.com/" + s3Message[1].detail.object.key);
     }
 
@@ -29,7 +29,7 @@ function Body(props: BodyProps) {
             />
             <div>
                 {(originalFileUrl)
-                    ? <AudioFilePlayer audioURL = {originalFileUrl}/>
+                    ? <AudioFilePlayer audioURL={originalFileUrl}/>
                     : "player goes here"
                 }
             </div>
