2da50c1d86e222fd09ee5c0748f0dee54f0b1483
Callum Spiller <callum.spiller@sony.com>
2023-03-02 09:51:24 +0000

    refactored use effects

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,48 +3,42 @@ import {v4 as uuidv4} from 'uuid';
 import "./stylesheets/styles.scss";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import {CreateQueueCommand, ReceiveMessageCommand, DeleteMessageCommand} from "@aws-sdk/client-sqs";
+import {CreateQueueCommand, DeleteMessageCommand, ReceiveMessageCommand} from "@aws-sdk/client-sqs";
 import {sqsClient} from "./libs/sqsClient";
 
-export default class App extends React.Component<{}, { uuid: string, separatedStems: string[] }> {
+export default class App extends React.Component<{}, { uuid: string }> {
     constructor(props: {}) {
         super(props);
         this.state = {
             uuid: uuidv4(),
-            separatedStems: []
         };
     }
 
     render() {
-        return (
-            <div id="react-app" className={"App"}>
-                <Header/>
-                <Body uuid={this.state.uuid}
-                      separatedStems={this.state.separatedStems}
-                      createSQSQueue={() => createSQSQueue(this.state.uuid)}
-                      getMessage={getMessage}
-                      deleteMessage={deleteMessage}
-                />
-            </div>
-        )
+        return (<div id="react-app" className={"App"}>
+            <Header/>
+            <Body uuid={this.state.uuid}
+                  createSQSQueue={() => createSQSQueue(this.state.uuid)}
+                  getMessage={getMessage}
+                  deleteMessage={deleteMessage}
+            />
+        </div>)
     }
 }
 
 async function createSQSQueue(uuid: string) {
     const params = {
-        QueueName: "upload_" + uuid,
-        Attributes: {
-            DelaySeconds: "0",
-            MessageRetentionPeriod: "86400"
+        QueueName: "upload_" + uuid, Attributes: {
+            DelaySeconds: "0", MessageRetentionPeriod: "86400"
         }
     };
 
     try {
         const data = await sqsClient.send(new CreateQueueCommand(params));
-        console.log("Success", data);
+        console.log("SQS Queue Created");
         return data;
     } catch (err) {
-        console.log("Error", err);
+        console.log("Error creating SQS Queue", err);
     }
 }
 
@@ -60,23 +54,21 @@ async function getMessage(queueURL: string) {
 
     try {
         const data = await sqsClient.send(new ReceiveMessageCommand(params));
-        console.log("Success, ", data);
         return data; // For unit tests.
     } catch (err) {
-        console.log("Error", err);
+        console.log("Error fetching message from SQS queue", err);
     }
 }
 
 async function deleteMessage(queueURL: string, receiptHandle: string) {
     const params = {
-        QueueUrl: queueURL,
-        ReceiptHandle: receiptHandle
+        QueueUrl: queueURL, ReceiptHandle: receiptHandle
     }
 
     try {
         const data = await sqsClient.send(new DeleteMessageCommand(params));
-        console.log("Message deleted", data);
+        console.log("Message deleted from queue", data);
     } catch (err) {
-        console.log("Error", err);
+        console.log("Error deleting message from queue", err);
     }
 }
--- ui/src/components/Body/index.tsx
+++ ui/src/components/Body/index.tsx
@@ -6,7 +6,6 @@ import {CircularProgress, Slider} from '@mui/material';
 
 interface BodyProps {
     uuid: string,
-    separatedStems: string[],
     createSQSQueue: Function,
     getMessage: Function,
     deleteMessage: Function
@@ -15,8 +14,10 @@ interface BodyProps {
 function Body(props: BodyProps) {
     const [uploadStatus, setUploadStatus] = useState<boolean>(false);
     const [sqsQueueUrl, setQueueUrl] = useState<string>();
-    const [fileUrls, setFileUrls] = useState<string[]>();
+    const [sqsMessage, setSQSMessage] = useState<ReceiveMessageResult>();
+    const [sqsMessageJson, setSQSMessageJson] = useState<any>();
     const [fileLabels, setFileLabels] = useState<string[]>([]);
+    const [fileUrls, setFileUrls] = useState<string[]>();
     const [spatialParams, setSpatialParams] = useState<any>();
 
     // Sets SQS URL after file is uploaded
@@ -27,139 +28,108 @@ function Body(props: BodyProps) {
             return result;
         }
 
-        if (uploadStatus && !sqsQueueUrl) {
+        if (!sqsQueueUrl) {
             createSqsQueue().then((result) => {
                 console.log(result)
             });
         }
-    }, [uploadStatus, sqsQueueUrl])
 
-    // Stores the URL of the created files and deletes the received message from the SQS queue.
+    }, [uploadStatus]);
+
+    // Fetches message from SQS Queue
     useEffect(() => {
-        async function setUploadedFileUrl() {
-            let message: ReceiveMessageResult = await props.getMessage(sqsQueueUrl);
-
-            // Wait for separation to occur
-            while (!message?.Messages) {
-                console.log("Retrying...")
-                message = await props.getMessage(sqsQueueUrl);
-            }
-
-            // Parse json string from message
-            const str: string = (message?.Messages && message?.Messages[0].Body) ? message.Messages[0].Body : "";
-            const bodyJson: any = JSON.parse(str);
-
-            console.log(bodyJson);
-            console.log(bodyJson.lambdaResult.Payload["output-paths"]);
-
-            const updateFileLabels = async (pathArray: string[]) => {
-                let pathArr: string[] = [];
-                for (let path of pathArray) {
-                    pathArr.push(path);
+        async function getMessageFromQueue() {
+            while (!sqsMessage?.Messages) {
+                setSQSMessage(await props.getMessage(sqsQueueUrl));
+                if (!sqsMessage?.Messages) {
+                    console.log("Still separating...");
                 }
-                setFileLabels(pathArr);
-            }
-
-            await updateFileLabels(bodyJson["lambdaResult"]["Payload"]["output-paths"])
-
-            console.log(fileLabels);
-
-            const spatialParamsSetup = async () => {
-                let spatialParamsSetup: any = {};
-                for (let label of fileLabels) {
-                    spatialParamsSetup[label] = {"X": 50, "Y": 50, "Z": 50};
-                }
-                setSpatialParams(spatialParamsSetup);
-            }
-
-            await spatialParamsSetup();
-            console.log(JSON.stringify(spatialParams));
-
-            const setupUrls = async () => {
-                let arr: string[] = [];
-                for (let path of bodyJson["lambdaResult"]["Payload"]["output-paths"]) {
-                    arr.push("https://" + bodyJson["lambdaResult"]["Payload"]["output-bucket"] + ".s3.eu-west-2.amazonaws.com/" + bodyJson["lambdaResult"]["Payload"]["output-folder"] + "/" + path)
-                }
-                setFileUrls(arr);
-            }
-
-            await setupUrls();
-            console.log(fileUrls);
-
-            // Delete fetched message from queue
-            if (message.Messages) {
-                await props.deleteMessage(sqsQueueUrl, message.Messages[0].ReceiptHandle);
             }
         }
 
-        if (sqsQueueUrl && !fileUrls) {
-            setUploadedFileUrl().then((result) => {
-                console.log(result);
-            });
+        getMessageFromQueue().then(() => {
+            console.log("Message fetched from queue")
+        });
+    }, [sqsQueueUrl])
+
+    useEffect(() => {
+        const str: string = (sqsMessage?.Messages && sqsMessage?.Messages[0].Body) ? sqsMessage.Messages[0].Body : "";
+        setSQSMessageJson(JSON.parse(str));
+    }, [sqsMessage])
+
+    useEffect(() => {
+        let pathArr: string[] = [];
+        for (let path of sqsMessageJson["lambdaResult"]["Payload"]["output-paths"]) {
+            pathArr.push(path);
         }
-    }, [sqsQueueUrl]);
+        setFileLabels(pathArr);
+    }, [sqsMessageJson])
+
+    useEffect(() => {
+        let spatialParamsSetup: any = {};
+        for (let label of fileLabels) {
+            spatialParamsSetup[label] = {"X": 50, "Y": 50, "Z": 50};
+        }
+        setSpatialParams(spatialParamsSetup);
+    }, [fileLabels])
+
+    useEffect(() => {
+        let arr: string[] = [];
+        for (let path of fileLabels) {
+            arr.push("https://" + sqsMessageJson["lambdaResult"]["Payload"]["output-bucket"] + ".s3.eu-west-2.amazonaws.com/" + sqsMessageJson["lambdaResult"]["Payload"]["output-folder"] + "/" + path)
+        }
+        setFileUrls(arr);
+    }, [fileLabels])
 
     const handleChange = (event: Event, newValue: number | number[], label: string, dimension: string) => {
         setSpatialParams({
-            ...spatialParams,
-            [label]: {
-                ...spatialParams[label],
-                [dimension]: newValue
+            ...spatialParams, [label]: {
+                ...spatialParams[label], [dimension]: newValue
             }
         });
     }
 
-    return (
-        <div>
+    return (<div>
             <Uploader
                 uuid={props.uuid}
                 setUploadStatus={() => setUploadStatus(true)}
             />
             <div>
-                {
-                    (uploadStatus && !fileUrls)
-                        ? <CircularProgress/> : <div/>
-                }
+                {(uploadStatus && !fileUrls) ? <CircularProgress/> : <div/>}
             </div>
 
             <div>
-                {
-                    (fileUrls)
-                        ? <ol>
-                            {fileUrls.map((url, index) => {
-                                return(
-                                <div>
-                                    <p>{fileLabels[index]}</p>
-                                    <AudioFilePlayer audioURL={url}/>
-                                    <div>
-                                        <Slider defaultValue={50} aria-label={fileLabels[index] + "_X"}
-                                                value={spatialParams[fileLabels[index]]["X"]} onChange={(e, newValue) => {
-                                            handleChange(e, newValue, fileLabels[index], "X")
-                                        }}/>
-                                        Set Value for forward/back
-                                    </div>
-                                    <div>
-                                        <Slider defaultValue={50} aria-label={fileLabels[index] + "_Y"}
-                                                value={spatialParams[fileLabels[index]]["Y"]} onChange={(e, newValue) => {
-                                            handleChange(e, newValue, fileLabels[index], "Y")
-                                        }}/>
-                                        Set Value for left/right
-                                    </div>
-                                    <div>
-                                        <Slider defaultValue={50} aria-label={fileLabels[index] + "_Y"}
-                                                value={spatialParams[fileLabels[index]]["Z"]} onChange={(e, newValue) => {
-                                            handleChange(e, newValue, fileLabels[index], "Z")
-                                        }}/>
-                                        Set Value for up/down
-                                    </div>
-                                </div>)
-                            })}
-                        </ol>
-                        : <div>"stems appear here"</div>
-                }
+                {(fileUrls) ? <ol>
+                    {fileUrls.map((url, index) => {
+                        return (<div>
+                            <p>{fileLabels[index]}</p>
+                            <AudioFilePlayer audioURL={url}/>
+                            <div>
+                                <Slider defaultValue={50} aria-label={fileLabels[index] + "_X"}
+                                        value={spatialParams[fileLabels[index]]["X"]} onChange={(e, newValue) => {
+                                    handleChange(e, newValue, fileLabels[index], "X")
+                                }}/>
+                                Set Value for forward/back
+                            </div>
+                            <div>
+                                <Slider defaultValue={50} aria-label={fileLabels[index] + "_Y"}
+                                        value={spatialParams[fileLabels[index]]["Y"]} onChange={(e, newValue) => {
+                                    handleChange(e, newValue, fileLabels[index], "Y")
+                                }}/>
+                                Set Value for left/right
+                            </div>
+                            <div>
+                                <Slider defaultValue={50} aria-label={fileLabels[index] + "_Y"}
+                                        value={spatialParams[fileLabels[index]]["Z"]} onChange={(e, newValue) => {
+                                    handleChange(e, newValue, fileLabels[index], "Z")
+                                }}/>
+                                Set Value for up/down
+                            </div>
+                        </div>)
+                    })}
+                </ol> : <div>"stems appear here"</div>}
             </div>
-        </div>
-    )
+        </div>)
 }
 
 export default Body;
