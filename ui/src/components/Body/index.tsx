aa115a9fb30127fd8924788888b9924ae3a88427
Callum Spiller <callum.spiller@sony.com>
2023-03-06 16:01:07 +0000

    reset styling

--- ui/src/components/Body/index.tsx
+++ ui/src/components/Body/index.tsx
@@ -1,4 +1,4 @@
-import React, {memo, useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Uploader from "../Upload";
 import {MemoAudioFilePlayer} from "../AudioFilePlayer";
 import {sfnClient} from "../../libs/stepFunctionsClient";
