36f22dc6af9ec1d95a6a18b9dcf8b0ebb5420ba7
Callum Spiller <callum.spiller@sony.com>
2023-03-13 17:05:34 +0000

    adjusting grid

--- ui/src/components/3dSpace/3dSpace.tsx
+++ ui/src/components/3dSpace/3dSpace.tsx
@@ -5,21 +5,16 @@ import { OrbitControls } from "@react-three/drei";
 
 function Box(props: ThreeElements["mesh"]) {
 	const ref = useRef<THREE.Mesh>(null!);
-	const [hovered, hover] = useState(false);
-	const [clicked, click] = useState(false);
 	useFrame((state, delta) => (ref.current.rotation.x));
 	return (
 		<mesh
 			{...props}
 			ref={ref}
-			scale={1}
-			onClick={(event) => click(!clicked)}
-			onPointerOver={(event) => hover(true)}
-			onPointerOut={(event) => hover(false)}>
+			scale={1}>
 			{/* eslint-disable-next-line react/no-unknown-property */}
 			<boxGeometry args={[20, 20, 20]} />
 			{/* eslint-disable-next-line react/no-unknown-property */}
-			<meshStandardMaterial color={hovered ? "hotpink" : "orange"} transparent={true} opacity={0.3}/>
+			<meshStandardMaterial color={"green"} transparent={true} opacity={0.3}/>
 		</mesh>
 	);
 }
