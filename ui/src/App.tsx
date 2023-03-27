f796ec406b9b1e6e5bb6e3c8d7f039df6915e931
Callum Spiller <callumjohnspiller@gmail.com>
2023-03-27 22:29:59 +0100

    new styles

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,5 @@
 import React, { type ReactElement } from "react";
 import { v4 as uuidv4 } from "uuid";
-import "./stylesheets/styles.scss";
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
