84f05c1a3654d47c8a5c11f90e162a4d68a3895f
Callum Spiller <callumjohnspiller@gmail.com>
2023-03-27 21:46:43 +0100

    trying new frontpage

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -9,7 +9,8 @@ import {
 	ReceiveMessageResult
 } from "@aws-sdk/client-sqs";
 import { sqsClient } from "./libs/sqsClient";
-import { Container } from "@mui/material";
+import {Button, Card, CardContent, Container, Slider, Typography} from "@mui/material";
+import './app.module.scss';
 
 export default class App extends React.Component<Record<string, unknown>, { uuid: string }> {
 	constructor (props: Record<string, unknown>) {
@@ -21,14 +22,73 @@ export default class App extends React.Component<Record<string, unknown>, { uuid
 
 	render (): ReactElement {
 		return (
-			<Container id={"app-container"}>
-				<Body uuid={this.state.uuid}
-					createSQSQueue={async () => await createSQSQueue(this.state.uuid)}
-					getMessage={getMessage}
-					deleteMessage={deleteMessage}
-				/>
-			</Container>
-		);
+			<div className="app">
+				<Container maxWidth="md">
+					<header className="app-header">
+						<Typography variant="h4" component="h1">
+							Welcome to our Spatial Audio Service
+						</Typography>
+					</header>
+					<div className="slide-container">
+						<div className="slide active">
+							<Card>
+								<CardContent>
+									<Typography gutterBottom variant="h5" component="h2">
+										Step 1
+									</Typography>
+									<Typography variant="body2" color="textSecondary" component="p">
+										Upload your audio file.
+									</Typography>
+								</CardContent>
+							</Card>
+						</div>
+						<div className="slide">
+							<Card>
+								<CardContent>
+									<Typography gutterBottom variant="h5" component="h2">
+										Step 2
+									</Typography>
+									<Typography variant="body2" color="textSecondary" component="p">
+										Choose your desired spatial audio settings.
+									</Typography>
+									<Slider
+										defaultValue={30}
+										aria-labelledby="discrete-slider"
+										valueLabelDisplay="auto"
+										step={10}
+										marks
+										min={10}
+										max={110}
+									/>
+								</CardContent>
+							</Card>
+						</div>
+						<div className="slide">
+							<Card>
+								<CardContent>
+									<Typography gutterBottom variant="h5" component="h2">
+										Step 3
+									</Typography>
+									<Typography variant="body2" color="textSecondary" component="p">
+										Download your newly created spatial audio file!
+									</Typography>
+									<Button variant="contained" color="primary">
+										Download
+									</Button>
+								</CardContent>
+							</Card>
+						</div>
+					</div>
+
+					<Body uuid={this.state.uuid}
+						  createSQSQueue={async () => await createSQSQueue(this.state.uuid)}
+						  getMessage={getMessage}
+						  deleteMessage={deleteMessage}
+					/>
+
+				</Container>
+			</div>
+		)
 	}
 }
 
