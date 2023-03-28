10e86fd34c173cabbd59be3e3c572b1618469d9a
Callum Spiller <callumjohnspiller@gmail.com>
2023-03-28 16:24:40 +0100

    argh

--- ui/src/components/3dSpace/Head-compressed.jsx
+++ ui/src/components/3dSpace/Head-compressed.jsx
@@ -11,7 +11,7 @@ import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export function Head(props) {
-  const { nodes, materials } = useGLTF('../../../public/prefabs/head/head.glb')
+  const { nodes, materials } = useGLTF('./scene.gltf')
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
