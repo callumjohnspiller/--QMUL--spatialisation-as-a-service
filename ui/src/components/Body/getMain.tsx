fda39cbae0a402a42e401e574f328d5f85122130
Callum Spiller <callum.spiller@sony.com>
2023-04-03 18:01:20 +0100

    fixed upload with no stem bug

--- ui/src/components/Body/getMain.tsx
+++ ui/src/components/Body/getMain.tsx
@@ -49,7 +49,7 @@ export function GetMain(
       {(uploadStatus && !fileUrls) && <CircularProgress sx={{position: 'absolute', top: "50%", left: "50%", height: "10vh", width: "10vw"}}/>}
       {(submitted && !outputUrl) && <CircularProgress sx={{position: 'absolute', top: "50%", left: "50%", height: "10vh", width: "10vw"}}/>}
       {(fileUrls && !submitted) &&
-          <div style={{ position: 'absolute', top: 30, right: 20, zIndex: 1 }}>
+          <div style={{ position: 'absolute', top: 30, right: 20, zIndex: 1, backgroundColor: "papayawhip", fillOpacity: 0.5 }}>
             <MemoHowlerGroup audioURLS={fileUrls} mutes={mutedChannels}/>
             <ul style={{width: 500}}>
               {fileUrls.map((url, index) => {
@@ -109,7 +109,7 @@ export function GetMain(
           </div>
       }
       {fileUrls && <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, zIndex: 0 }}><Scene spatialParams={spatialParams} fileLabels={fileLabels} /></div>}
-      {outputUrl && <div style={{ position: 'absolute', bottom: 10, left: 10, zIndex: 2 }}><MemoAudioFilePlayer audioURL={outputUrl}/></div>}
+      {outputUrl && <div style={{ position: 'absolute', top: 30, right: 20, zIndex: 1, backgroundColor: "papayawhip", fillOpacity: 0.5 }}><MemoAudioFilePlayer audioURL={outputUrl}/></div>}
     </div>
   );
 }
