91fed5031f9e50d9619ee9897725b3846a0c047b
Callum Spiller <callumjohnspiller@gmail.com>
2023-04-24 19:56:41 +0100

    and screen shots

---
+++ ui/playwright/index.tsx
@@ -0,0 +1,14 @@
+// Import styles, initialize component theme here.
+// import '../src/common.css';
+
+import { beforeMount, afterMount } from '@playwright/experimental-ct-react/hooks';
+import { BrowserRouter } from 'react-router-dom';
+
+export type HooksConfig = {
+    enableRouting?: boolean;
+}
+
+beforeMount<HooksConfig>(async ({ App, hooksConfig }) => {
+    if (hooksConfig?.enableRouting)
+        return <BrowserRouter><App/></BrowserRouter>;
+});
